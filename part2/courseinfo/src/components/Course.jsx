ba6680b9b60600e48763a4ebac3fd426163e0085
elliottfeltham <elliottfeltham98@gmail.com>
2025-08-18 22:57:13 +0100

    Exercise 2.5 complete

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,36 @@
+const Header = (props) => <h1>{props.course}</h1>;
+
+const Content = ({ parts }) => (
+	<div>
+		{parts.map((part) => (
+			<Part key={part.id} part={part} />
+		))}
+	</div>
+);
+
+const Part = (props) => (
+	<p>
+		{props.part.name} {props.part.exercises}
+	</p>
+);
+
+const Total = ({ parts }) => (
+	<p>
+		<strong>
+			Total of exercises{" "}
+			{parts.reduce((sum, part) => sum + part.exercises, 0)}
+		</strong>
+	</p>
+);
+
+const Course = ({ course }) => {
+	return (
+		<div>
+			<Header course={course.name} />
+			<Content parts={course.parts} />
+			<Total parts={course.parts} />
+		</div>
+	);
+};
+
+export default Course;

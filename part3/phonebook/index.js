d87b561d50ac28f5cbecff332dd9e6cc687c8fd4
elliottfeltham <elliottfeltham98@gmail.com>
2025-09-15 21:37:36 +0100

    Exercise 3.5 corrected, Exercise 3.5 complete

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -58,7 +58,7 @@ app.post("/api/persons", (req, res) => {
 
 app.delete("/api/persons/:id", (req, res) => {
 	const id = req.params.id;
-	persons = persons.filter((p) => p.id === id);
+	persons = persons.filter((p) => p.id !== id);
 
 	res.status(204).end();
 });

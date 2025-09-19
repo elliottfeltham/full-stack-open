0c85d2e6905999af04ef541f2ab906b1fb577b66
elliottfeltham <elliottfeltham98@gmail.com>
2025-09-19 23:46:34 +0100

    Exercise 4.7 complete

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,7 +1,107 @@
-const dummy = (blogs) => {
-	return 1;
+const _ = require("lodash");
+
+const totalLikes = (array) => {
+	if (array.length === 0) {
+		return 0;
+	}
+
+	return array.reduce((sum, item) => sum + item.likes, 0);
 };
 
+const favouriteBlog = (array) => {
+	if (array.length === 0) {
+		return 0;
+	}
+
+	return array.reduce((most, current) => {
+		return current.likes >= most.likes ? current : most;
+	});
+};
+
+const mostBlogs = (array) => {
+	if (array.length === 0) {
+		return 0;
+	}
+
+	const counts = _.countBy(array, "author");
+
+	const topAuthor = _.maxBy(Object.keys(counts), (author) => counts[author]);
+
+	return {
+		author: topAuthor,
+		blogs: counts[topAuthor],
+	};
+};
+
+const mostLikes = (blogs) => {
+	if (blogs.length === 0) return 0;
+
+	const groupedAuthors = _.groupBy(blogs, "author");
+
+	const totals = _.map(groupedAuthors, (authorBlogs, author) => ({
+		author,
+		likes: _.sumBy(authorBlogs, "likes"),
+	}));
+
+	return _.maxBy(totals, "likes");
+};
+
+const blogs = [
+	{
+		_id: "5a422a851b54a676234d17f7",
+		title: "React patterns",
+		author: "Michael Chan",
+		url: "https://reactpatterns.com/",
+		likes: 7,
+		__v: 0,
+	},
+	{
+		_id: "5a422aa71b54a676234d17f8",
+		title: "Go To Statement Considered Harmful",
+		author: "Edsger W. Dijkstra",
+		url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+		likes: 5,
+		__v: 0,
+	},
+	{
+		_id: "5a422b3a1b54a676234d17f9",
+		title: "Canonical string reduction",
+		author: "Edsger W. Dijkstra",
+		url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+		likes: 12,
+		__v: 0,
+	},
+	{
+		_id: "5a422b891b54a676234d17fa",
+		title: "First class tests",
+		author: "Robert C. Martin",
+		url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+		likes: 10,
+		__v: 0,
+	},
+	{
+		_id: "5a422ba71b54a676234d17fb",
+		title: "TDD harms architecture",
+		author: "Robert C. Martin",
+		url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+		likes: 0,
+		__v: 0,
+	},
+	{
+		_id: "5a422bc61b54a676234d17fc",
+		title: "Type wars",
+		author: "Robert C. Martin",
+		url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+		likes: 2,
+		__v: 0,
+	},
+];
+
+console.log(mostBlogs(blogs));
+
 module.exports = {
-	dummy,
+	totalLikes,
+	favouriteBlog,
+	mostBlogs,
+	mostLikes,
 };
